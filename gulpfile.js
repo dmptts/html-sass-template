2257e2312394ad1ce37ff3d4b4c1b914c0169011
Dmitriy Solovev <dmptts@gmail.com>
2022-08-09 13:48:33 +0300

    Настраивает линтеры, Prettier и реализует задачу по обработке .scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,14 @@
 const gulp = require('gulp')
-const webpack = require('webpack')
 const webpackStream = require('webpack-stream')
 const webpackConfig = require('./webpack.config')
 const browserSync = require('browser-sync').create()
+const plumber = require('gulp-plumber')
+const sass = require('gulp-sass')(require('sass'))
+const autoprefixer = require('gulp-autoprefixer')
+const sourcemap = require('gulp-sourcemaps')
+const cgmq = require('gulp-group-css-media-queries')
+const csso = require('gulp-csso')
+const rename = require('gulp-rename')
 
 const js = () => {
   return gulp
@@ -15,6 +21,21 @@ const html = () => {
   return gulp.src('./src/html/**/*.html').pipe(gulp.dest('./dist'))
 }
 
+const scss = () => {
+  return gulp
+    .src('./src/scss/main.scss')
+    .pipe(plumber())
+    .pipe(sourcemap.init())
+    .pipe(sass())
+    .pipe(autoprefixer())
+    .pipe(cgmq())
+    .pipe(gulp.dest('./dist/scss'))
+    .pipe(csso())
+    .pipe(rename('main.min.css'))
+    .pipe(sourcemap.write('.'))
+    .pipe(gulp.dest('./dist/scss'))
+}
+
 const serve = () => {
   browserSync.init({
     server: { baseDir: './dist' },
@@ -23,9 +44,10 @@ const serve = () => {
 
   gulp.watch('src/html/**/*.html', gulp.series(html, browserSync.reload))
   gulp.watch('src/js/**/*.js', gulp.series(js, browserSync.reload))
+  gulp.watch('src/scss/**/*.scss', gulp.series(scss, browserSync.stream))
 }
 
-const build = gulp.series(js, html)
+const build = gulp.series(js, html, scss)
 const start = gulp.series(build, serve)
 
 exports.build = build
