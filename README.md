a755396e0a368cc1d9bbca2260658dfbc8243146
Dmitriy Solovev <dmptts@gmail.com>
2022-08-11 10:54:44 +0300

    Добавляет обработку изображений, базовые стили, подключает normalize.css, создание svg-спрайта

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# Ultimate Webpack 5 + Gulp 5 config
+# HTMl + SCSS template
+
+Команды:
+
+- `npm start` - запускает localhost в dev-режиме;
+- `npm run dev` - собирает проект в dev-режиме;
+- `npm run build` - собирает проект в production-режиме;
+- `gulp createWebp` - форматирует .jpeg, .jpg, .png изображения из папки `src/img/content` в .webp
--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,9 @@ const sourcemap = require('gulp-sourcemaps')
 const cgmq = require('gulp-group-css-media-queries')
 const csso = require('gulp-csso')
 const rename = require('gulp-rename')
+const svgstore = require('gulp-svgstore')
+const imagemin = require('gulp-imagemin')
+const webp = require('gulp-webp')
 
 const js = () => {
   return gulp
@@ -36,6 +39,59 @@ const scss = () => {
     .pipe(gulp.dest('./dist/scss'))
 }
 
+const optimizeImg = () => {
+  return gulp
+    .src('./src/img/content/*.{jpeg,jpg,png,gif,webp}')
+    .pipe(
+      imagemin({
+        verbose: true,
+      })
+    )
+    .pipe(gulp.dest('./dist/img'))
+}
+
+const copyImg = () => {
+  return gulp
+    .src('./src/img/content/**/*.{jpeg,jpg,png,gif,webp}')
+    .pipe(gulp.dest('./dist/img'))
+}
+
+const fonts = () => {
+  return gulp.src('src/fonts/**').pipe(gulp.dest('dist/fonts'))
+}
+
+const svgo = () => {
+  return gulp
+    .src('src/img/**/*.svg')
+    .pipe(
+      imagemin([
+        imagemin.svgo({
+          plugins: [
+            { removeViewBox: false },
+            { removeRasterImages: true },
+            { removeUselessStrokeAndFill: false },
+          ],
+        }),
+      ])
+    )
+    .pipe(gulp.dest('src/img'))
+}
+
+const sprite = () => {
+  return gulp
+    .src('src/img/sprite/*.svg')
+    .pipe(svgstore({ inlineSvg: true }))
+    .pipe(rename('sprite.svg'))
+    .pipe(gulp.dest('dist/img'))
+}
+
+const createWebp = () => {
+  return gulp
+    .src('src/img/content/*.{jpeg,jpg,png}')
+    .pipe(webp())
+    .pipe(gulp.dest('src/img/content'))
+}
+
 const serve = () => {
   browserSync.init({
     server: { baseDir: './dist' },
@@ -45,10 +101,16 @@ const serve = () => {
   gulp.watch('src/html/**/*.html', gulp.series(html, browserSync.reload))
   gulp.watch('src/js/**/*.js', gulp.series(js, browserSync.reload))
   gulp.watch('src/scss/**/*.scss', gulp.series(scss, browserSync.stream))
+  gulp.watch('src/img/content/**/*.{jpeg,jpg,png,gif}'),
+    gulp.series(copyImg, browserSync.reload)
+  gulp.watch('src/img/sprite/**/*.svg'), gulp.series(sprite, browserSync.reload)
 }
 
-const build = gulp.series(js, html, scss)
-const start = gulp.series(build, serve)
+const build = gulp.series(js, html, scss, optimizeImg, svgo, sprite, fonts)
+const dev = gulp.series(js, html, scss, copyImg, sprite, fonts)
+const start = gulp.series(dev, serve)
 
+exports.webp = createWebp
 exports.build = build
+exports.dev = dev
 exports.start = start
